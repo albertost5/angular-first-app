054d841f35a382914ed06b6cd38bcaad38d39b9e
albertost5 <sancheztorresalberto@gmail.com>
2023-03-18 18:13:20 +0100

    Add counter folder

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-counter',
+    templateUrl: './counter.component.html',
+    styleUrls: ['./counter.component.css'],
+})
+export class CounterComponent {
+    title: string = 'Counter App';
+    counter: number = 0;
+    base: number = 5;
+
+    operation(value: number) {
+        if (value > 0 || (value < 0 && this.counter > 0))
+            this.counter += value;
+    }
+}
