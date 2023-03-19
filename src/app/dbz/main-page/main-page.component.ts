d6a0770680ed3304e9d3ea4895e0a6b869e62c18
albertost5 <sancheztorresalberto@gmail.com>
2023-03-19 17:45:38 +0100

    Add character component, interface and share data between parent-child components

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-interface Character {
-  name: string;
-  power: number;
-}
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-main-page',
